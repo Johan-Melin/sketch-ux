7003b1ac3edc0ba5a95b6da8e2a8feb7e08540d7
Johan Melin <soldierheart@hotmail.com>
2024-01-04 22:59:23 +0100

    add back button screeneditor

--- src/components/ScreenEditor.jsx
+++ src/components/ScreenEditor.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react';
 import TopBar from './ScreenEditor/TopBar';
 import CanvasArea from './ScreenEditor/Canvas';
 
-const ScreenEditor = () => {
-  const [canvasText, setCanvasText] = useState('');
+import PropTypes from 'prop-types';
 
-  return (
-    <div>
-      <TopBar setCanvasText={setCanvasText} />
-      <CanvasArea canvasText={canvasText} />
-    </div>
-  );
+const ScreenEditor = ({ onBackToProjects }) => {
+    const [canvasText, setCanvasText] = useState('');
+
+    return (
+        <div>
+            <TopBar setCanvasText={setCanvasText} onBackToProjects={onBackToProjects} />
+            <CanvasArea canvasText={canvasText} />
+        </div>
+    );
+};
+
+ScreenEditor.propTypes = {
+    onBackToProjects: PropTypes.func.isRequired,
 };
 
 export default ScreenEditor;
--- src/components/ScreenEditor/TopBar.jsx
+++ src/components/ScreenEditor/TopBar.jsx
@@ -1,21 +1,23 @@
 import PropTypes from 'prop-types';
 
-function TopBar({ setCanvasText }) {
-        const handleButtonClick = (text) => {
-            setCanvasText(text);
-        };
-    
-        return (
-            <div>
-                <button onClick={() => handleButtonClick('Text from Button 1')}>Button 1</button>
-                <button onClick={() => handleButtonClick('Text from Button 2')}>Button 2</button>
-                <button onClick={() => handleButtonClick('Text from Button 3')}>Button 3</button>
-            </div>
-        );
-    }
+function TopBar({ setCanvasText, onBackToProjects }) {
+    const handleButtonClick = (text) => {
+        setCanvasText(text);
+    };
+
+    return (
+        <div>
+            <button onClick={onBackToProjects}>Back</button>
+            <button onClick={() => handleButtonClick('Text from Button 1')}>Button 1</button>
+            <button onClick={() => handleButtonClick('Text from Button 2')}>Button 2</button>
+            <button onClick={() => handleButtonClick('Text from Button 3')}>Button 3</button>
+        </div>
+    );
+}
 
 TopBar.propTypes = {
     setCanvasText: PropTypes.func.isRequired,
+    onBackToProjects: PropTypes.func.isRequired,
 };
 
 export default TopBar;
