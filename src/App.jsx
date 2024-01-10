d2c7729bf6597639a9ff11d852f3576b2d2a8626
Johan Melin <soldierheart@hotmail.com>
2024-01-10 14:04:16 +0100

    prevent swipe refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import ProjectMenu from './components/ProjectMenu';
 import ScreenEditor from './components/ScreenEditor';
 import useProjectManager from './hooks/useProjectManager';
 
 function App() {
   const { currentScreen, selectScreen, handleBackToProjects } = useProjectManager();
+
+  useEffect(() => {
+    //prevent refresh page on swipe down
+    const preventDefault = (e) => e.preventDefault();
+    document.addEventListener('touchmove', preventDefault, { passive: false });
+  
+    return () => {
+      document.removeEventListener('touchmove', preventDefault);
+    };
+  }, []);
 
   return (
     <div>
