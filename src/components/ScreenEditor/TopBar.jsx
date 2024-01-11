535cf50aa0f54ac16c818cd201191184f36c3ebb
Johan Melin <soldierheart@hotmail.com>
2024-01-11 22:08:25 +0100

    add edit toggle topbar

--- src/components/ScreenEditor/TopBar.jsx
+++ src/components/ScreenEditor/TopBar.jsx
@@ -1,26 +1,41 @@
 import PropTypes from 'prop-types';
 import TopBarContext from '../../context/TopBarContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import styles from './TopBar.module.css';
 import TopBarButton from './TopBarButton';
 import { TOOLS, ACTIONS } from '../../constants/tools';
-import { FaArrowLeft, FaFont, FaSquare, FaRegSquare, FaUndo, FaTrash } from 'react-icons/fa'; 
+import { FaArrowLeft, FaFont, FaSquare, FaRegSquare, FaUndo, FaRegObjectUngroup, FaHandPointer, FaTrash, FaFile } from 'react-icons/fa'; 
 function TopBar({ onBackToProjects }) {
     const { setSelectedTool, handleAction } = useContext(TopBarContext);
     const { TEXT, IMAGE, INPUT } = TOOLS;
     const { UNDO, CLEAR } = ACTIONS;
+    const [isEditMode, setIsEditMode] = useState(false);
+    
+    const handleEditClick = () => {
+        setIsEditMode(!isEditMode);
+    };
 
     return (
             <div className={styles.topBar}>
                 <div className={styles.row}>
                     <TopBarButton name="back" handleClick={onBackToProjects} Icon={FaArrowLeft} />
-                    <TopBarButton name="text" handleClick={() => setSelectedTool(TEXT)} Icon={FaFont} />
-                    <TopBarButton name="image" handleClick={() => setSelectedTool(IMAGE)} Icon={FaSquare} />
-                    <TopBarButton name="input" handleClick={() => setSelectedTool(INPUT)} Icon={FaRegSquare} />
+                    {isEditMode ? (
+                        <>
+                            <TopBarButton name="select" handleClick={() => []} Icon={FaHandPointer} />
+                            <TopBarButton name="delete" handleClick={() => []} Icon={FaTrash} />
+                        </>
+                    ) : (
+                    <>
+                        <TopBarButton name="text" handleClick={() => setSelectedTool(TEXT)} Icon={FaFont} />
+                        <TopBarButton name="image" handleClick={() => setSelectedTool(IMAGE)} Icon={FaSquare} />
+                        <TopBarButton name="input" handleClick={() => setSelectedTool(INPUT)} Icon={FaRegSquare} />
+                    </>
+                )}
                 </div>
                 <div className={styles.row}>
+                    {isEditMode && <TopBarButton name="clear" handleClick={() => handleAction(CLEAR)} Icon={FaFile} />}
                     <TopBarButton name="undo" handleClick={() => handleAction(UNDO)} Icon={FaUndo} />
-                    <TopBarButton name="clear" handleClick={() => handleAction(CLEAR)} Icon={FaTrash} />
+                    <TopBarButton name="edit" handleClick={handleEditClick} Icon={FaRegObjectUngroup} />
                 </div>
         </div>
     );
