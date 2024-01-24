9d98a69486b08ba9ad8e4853806e745f3f9b9beb
Johan Melin <soldierheart@hotmail.com>
2024-01-24 01:12:33 +0100

    add go to screen from linklist

--- src/components/ScreenEditor/LinkList.jsx
+++ src/components/ScreenEditor/LinkList.jsx
@@ -3,12 +3,13 @@ import { ProjectsContext } from "../../context/ProjectsContext";
 import { useContext } from 'react';
 import useScreenActions from '../../hooks/useScreenActions';
 import TopBarContext from '../../context/TopBarContext';
+import { FaArrowRight } from 'react-icons/fa';
 
 function LinkList() {
-    const { projects, currentProjectId } = useContext(ProjectsContext);
+    const { projects, currentProjectId, currentScreenId, setCurrentScreenId } = useContext(ProjectsContext);
     const currentProject = projects.find(project => project.id === currentProjectId);    
     const { addScreen, copyScreen, editScreen, addLink } = useScreenActions();
-    const { selectedRect, setSelectedRect } = useContext(TopBarContext);   
+    const { selectedRect, setSelectedRect, setDisplayModal } = useContext(TopBarContext);   
 
     const handleAddScreen = () => {
         const id = addScreen();
@@ -26,6 +27,11 @@ function LinkList() {
         addLink(screenId, selectedRect.id);
     }
 
+    const handleGoToScreen = () => {
+        setDisplayModal(null);
+        setCurrentScreenId(selectedRect.link);
+    }
+
     return (
         <div className={styles.container}>
             <p>Link to:</p>
@@ -33,11 +39,13 @@ function LinkList() {
                 <div className={styles.item} onClick={handleAddScreen}>New Screen</div>
                 <div className={styles.item} onClick={handleCopyScreen}>Copy of current screen</div>
             </div>
-            {currentProject.screens.map((screen) => {
+            {currentProject.screens
+            .filter(screen => screen.id !== currentScreenId)
+            .map((screen) => {
                 const isSelected = selectedRect && screen.id === selectedRect?.link;
                 return (
                     <div key={screen.id} className={`${styles.item} ${isSelected ? styles.selected : ''}`} onClick={() => handleAddLink(screen.id)}>
-                        {screen.name}  {isSelected && '✓'}
+                        {screen.name} {isSelected && <FaArrowRight onClick={handleGoToScreen}/>}
                     </div>
                 )}
             )}
